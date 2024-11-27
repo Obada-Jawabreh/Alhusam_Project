b1ffb23ff7e93700aa7ad1472b55fa0d514a7ef2
Dua'a Khamis <your_email@example.com>
2024-11-27 18:11:08 +0300

    add

---
+++ Back-End/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profilePicture: String,
+    role: {
+      type: String,
+      enum: ["user", "provider","driver"],
+      default: "user",
+    },
+    bio: String,
+    savedBooks: [{ type: Schema.Types.ObjectId, ref: "Book" }],
+    publishedBooks: [{ type: Schema.Types.ObjectId, ref: "Book" }],
+    googleId: { type: String },
+    facebookId: { type: String },
+    isDeleted: { type: Boolean, default: false },
+    isActivated: { type: Boolean, default: false }, // Default to false
+    otp: String, 
+    otpExpiry: Date,
+  },
+  { timestamps: true }
+);
+const User = mongoose.models.User || mongoose.model("User", userSchema, "Users");
+module.exports = User;
